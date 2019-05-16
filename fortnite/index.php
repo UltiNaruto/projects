a0448387d89f88d4100dfb9fab46881812ed58a9
UltiNaruto <zenaruto@ymail.com>
2019-05-16 22:34:26 +0200

    Create index.php

---
+++ fortnite/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<HTML>
+	<HEAD>
+		<META HTTP-EQUIV="content-style-type" CONTENT="text/css">
+		<META CHARSET="UTF-8">
+		<TITLE>Fortnite - Ile du mode créatif - Liste</TITLE>
+	</HEAD>
+	<BODY>
+	</BODY>
+</HTML>
